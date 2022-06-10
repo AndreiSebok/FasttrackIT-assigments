a83b0b958d3d71b5fce08a62cf69ddb2045b9599
AndreiSebok <100995234+AndreiSebok@users.noreply.github.com>
2022-06-10 22:50:28 +0100

    Add files via upload

---
+++ C18/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import axios from 'axios'
+
+function App() {
+
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=8b1016781add2f2c9675e561a87fe8e8`
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
